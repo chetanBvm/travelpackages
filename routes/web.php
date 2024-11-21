453420bbfe48943d0e20f667204f12d6850a1671
vikas-bvm <khetaram.bvmsolutions@gmail.com>
2024-11-21 18:34:51 +0530

    theme setup and authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\Admin\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/dashboard',function(){
+    return view('admin.dashboard');
+});
+
+//Admin
+Route::group(['prefix' => 'admin'], function () {
+
+    Route::resource('user', UserController::class);
 });
