d058b6466d8edf714dbd830e88944697fbc4ed57
ShubhamBVMWebSolutions <shus.bvmsolutions@gmail.com>
2024-11-25 16:47:20 +0530

    design admin panel

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -40,7 +40,41 @@
 
     <!-- Vendors CSS -->
     <link rel="stylesheet" href="{{asset('admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}" />
+    <link rel="stylesheet" href="{{asset('admin/assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css')}}" />
+    <link rel="stylesheet" href="{{asset('admin/assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css')}}" />
+    <link rel="stylesheet" href="{{asset('admin/assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css')}}" />
+    <link rel="stylesheet" href="{{asset('admin/assets/vendor/libs/datatables-buttons-bs5/buttons.bootstrap5.css')}}" />
+    <link rel="stylesheet" href="{{asset('admin/assets/vendor/libs/flatpickr/flatpickr.css')}}" />
     <link rel="stylesheet" href="{{asset('admin/assets/vendor/libs/apex-charts/apex-charts.css')}}" />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Row Group CSS -->
+<link rel="stylesheet" href="{{asset('admin/assets/vendor/libs/datatables-rowgroup-bs5/rowgroup.bootstrap5.css')}}" />
+
+
+
+<!-- Form Validation -->
+<link rel="stylesheet" href="{{asset('admin/assets/vendor/libs/@form-validation/form-validation.css')}}" />
+
 
     <!-- Page CSS -->
 
@@ -56,17 +90,17 @@
     <div class="layout-wrapper layout-content-navbar">
       <div class="layout-container">
         <!-- BEGIN: Menu -->
-        
+
         @include('admin.layouts.menu')
-          
-        
+
+
         <!-- END: Menu -->
         <!-- Layout container -->
         <div class="layout-page">
             <!-- Navbar -->
-            
+
                 @include('admin.layouts.navbar')
-        
+
             <!-- / Navbar -->
 
           <!-- Content wrapper -->
@@ -82,7 +116,7 @@
 
             <!-- Footer -->
                 @include('admin.layouts.footer')
-            
+
             <!-- / Footer -->
 
             <div class="content-backdrop fade"></div>
@@ -98,24 +132,73 @@
     <!-- / Layout wrapper -->
 
     <!-- Core JS -->
+
+
     <!-- build:js assets/vendor/js/core.js -->
     <script src="{{asset('admin/assets/vendor/libs/jquery/jquery.js')}}"></script>
     <script src="{{asset('admin/assets/vendor/libs/popper/popper.js')}}"></script>
     <script src="{{asset('admin/assets/vendor/js/bootstrap.js')}}"></script>
     <script src="{{asset('admin/assets/vendor/libs/node-waves/node-waves.js')}}"></script>
     <script src="{{asset('admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
+    <script src="{{asset('admin/assets/vendor/libs/hammer/hammer.js')}}"></script>
+    <script src="{{asset('admin/assets/vendor/libs/i18n/i18n.js')}}"></script>
+    <script src="{{asset('admin/assets/vendor/libs/typeahead-js/typeahead.js')}}"></script>
     <script src="{{asset('admin/assets/vendor/js/menu.js')}}"></script>
+
+
+
+
+
 
     <!-- endbuild -->
 
     <!-- Vendors JS -->
+    <script src="{{asset('admin/assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js')}}"></script>
     <script src="{{asset('admin/assets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
+
+
+
+
+<!-- Form Validation -->
+
+<script src="{{asset('admin/assets/vendor/libs/@form-validation/popular.js')}}"></script>
+<script src="{{asset('admin/assets/vendor/libs/@form-validation/bootstrap5.js')}}"></script>
+<script src="{{asset('admin/assets/vendor/libs/@form-validation/auto-focus.js')}}"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     <!-- Main JS -->
     <script src="{{asset('admin/assets/js/main.js')}}"></script>
 
+
+
+
+
+
+    {{-- <script src="{{asset('admin/assets/js/modal-edit-user.js')}}"></script> --}}
+
     <!-- Page JS -->
     <script src="{{asset('admin/assets/js/dashboards-analytics.js')}}"></script>
+
+    <script src="{{asset('admin/assets/js/tables-datatables-basic.js')}}"></script>
+
 
     <!-- Place this tag before closing body tag for github widget button. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
