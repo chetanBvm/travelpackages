159c9a3f8adfc4fee77508db9ad0c6b0fb59c937
chetanBvm <chetan.bvmsolutions@gmail.com>
2024-11-25 10:21:38 +0530

    few

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 //Admin
 Route::group(['prefix' => 'admin','middelware'=> ['auth']], function () {
